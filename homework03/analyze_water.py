a80707f24709653152d721103767d9089fe981b8
Bryan Acosta <70548114+bryan4027@users.noreply.github.com>
2022-02-15 22:19:56 -0600

    Update analyze_water.py

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -1,1 +1,40 @@
 import json
+import math
+
+
+#def calculate_turbidity():
+
+#def caclulate_minimum_time():
+
+def calculate_turbidity(datavec):
+    x = -5
+    global lastfivedicts
+    lastfivedicts = []
+    for i in range(0,5):
+        lastfivedicts.append(datavec[x])
+        x = x+1
+    global avgturb
+    avgturb = 0
+    for i in range(0,5):
+        calibration_const = float(lastfivedicts[i]['calibration_constant'])
+        current = float(lastfivedicts[i]['detector_current'])
+        avgturb = avgturb + (calibration_const*current)
+    avgturb = avgturb/(len(lastfivedicts))
+    print(avgturb)
+    return avgturb
+
+with open('turbidity_data.json', 'r') as f:
+    datavec = json.load(f)
+
+calculate_turbidity(datavec['turbidity_data'])
+
+for i in lastfivedicts:
+    print(i)
+
+#def main():
+
+
+
+
+#if __name__ == '__main__':
+ #   main()
