e4d5518bcb2458398ef5373373fc8ded4b271b10
Bryan Acosta <b.acosta@utexas.edu>
2022-02-17 03:50:32 -0600

    updated files

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -1,15 +1,28 @@
 import json
 import math
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 def calculate_turbidity(datavec: list) -> float:
+    '''
+    This function:
+         This function reads in a list of data points recorded real time, takes the last 5 data points and calculates the average Turbidity and returns it
+
+    Args:
+         datavec (list): The input is a list of dictionaries with the following values: datetime - a date in the format of a string, sample_volume - a double, calibration_constant - a double, detector_current - a double,  analyzed_by - a string
+
+    Returns:
+         this function returns the average turbidity of the last five data points recorded
+
+    '''
     x = -5
     global lastfivedicts
     lastfivedicts = []
+    
     for i in range(0,5):
-        lastfivedicts.append(datavec[x])
-        x = x+1
+            lastfivedicts.append(datavec[x])
+            x = x+1
     avgturb = 0
-    for i in range(0,5):
+    for i in range(0,len(lastfivedicts)):
         calibration_const = float(lastfivedicts[i]['calibration_constant'])
         current = float(lastfivedicts[i]['detector_current'])
         avgturb = avgturb + (calibration_const*current)
@@ -18,6 +31,17 @@ def calculate_turbidity(datavec: list) -> float:
     
 
 def calculate_minimum_time(avgturb: float) -> float:
+'''
+    This function:
+         This function reads in the average turbidity of the 5 most recent data points, uses the given equation to calculate the minimum amount of time required, and returns it
+
+    Args:
+         avgturb is the average turbidity of the 5 most recent data points. it is always a float.
+
+    Returns:
+         this function returns the the amount of time that it takes for the turbidity to get to a safe value. 
+
+'''
     global hour
     hour = 0
     hour  = math.log((1/avgturb),0.98)
@@ -26,10 +50,10 @@ def calculate_minimum_time(avgturb: float) -> float:
 def printstuff(avgturb: float, time: float) ->None:
     print("\n     Avg Turbidity: ",avgturb)
     if (avgturb >= 1):
-        print("     Warning: Turbidity is above threshold for safe use ")
+        logging.warning('Warning: Turbidity is above threshold for safe use ')
         print ("     Minimum time required to return below a safe threshold = ",hour, "\n")
     if (avgturb < 1):
-        print ("     Turbidity is safe for use. \n" )
+        logging.info ('Turbidity is safe for use. \n' )
     
     
 def main():
--- homework03/test_analyze_water.py
+++ homework03/test_analyze_water.py
@@ -5,14 +5,19 @@ from analyze_water import calculate_minimum_time
 from analyze_water import main
 
 def test_calculate_turbidity():
-    with open('testdata.json', 'r') as f:
+    with open('datatest.json', 'r') as f:
         testdata = json.load(f)
+    with open('datatest2.json', 'r') as f:
+        testdata2 = json.load(f)
         
+
     assert(calculate_turbidity(testdata['turbidity_data']) == 1.1992)
     assert isinstance(calculate_turbidity(testdata['turbidity_data']), float) == True
-    with pytest.raises(ValueError):
-            calculate_turbidity(5)
-    
+    assert isinstance(calculate_turbidity(testdata['turbidity_data']), str) != True
+    assert(calculate_turbidity(testdata2['turbidity_data']) == 1.4632)
+    assert(calculate_turbidity(testdata2['turbidity_data']) != 1.8482)
+
+1.4632    
     #assert isinstance(calculate_turbidity([{'a': 1}, {'a': 2}], 'a'), float)
     #assert isinstance(calculate_turbidity([{'a': 1}, {'a': 2}], 'a'), float)
 
@@ -21,6 +26,14 @@ def test_calculate_minimum_time():
     assert(calculate_minimum_time(1.1992) == 8.991600232149228)
     assert(calculate_minimum_time(1.3422) == 14.567852422768159)
     assert(calculate_minimum_time(1.4244) == 17.510062728806584)
+    assert isinstance(calculate_minimum_time(1.4244), float) == True
+    assert isinstance(calculate_minimum_time(1.4244), str) == False
 
-test_calculate_turbidity()
-test_calculate_minimum_time()
+def main():
+
+    test_calculate_turbidity()
+    test_calculate_minimum_time()
+
+
+if __name__ == '__main__':
+    main()
