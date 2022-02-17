899e70d5602d7e776f03f0e3dd3e53b1ad1dc35e
Bryan Acosta <70548114+bryan4027@users.noreply.github.com>
2022-02-16 22:08:40 -0600

    Update analyze_water.py

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -3,37 +3,46 @@ import math
 
 def calculate_turbidity(datavec: list) -> float:
     x = -5
-    global lastfivedicts
+    lastfivedicts
     lastfivedicts = []
     for i in range(0,5):
         lastfivedicts.append(datavec[x])
         x = x+1
-    global avgturb
+    avgturb
     avgturb = 0
     for i in range(0,5):
         calibration_const = float(lastfivedicts[i]['calibration_constant'])
         current = float(lastfivedicts[i]['detector_current'])
         avgturb = avgturb + (calibration_const*current)
     avgturb = avgturb/(len(lastfivedicts))
+    return avgturb
+    
+
+def calculate_minimum_time(avgturb: float) -> float:
+    global hour
+    hour = 0
+    hour  = math.log((1/avgturb),0.98)
+    return hour
+
+def printstuff(avgturb: float, time: float) {
     print("\n     Avg Turbidity: ",avgturb)
     if (avgturb >= 1):
         print("     Warning: Turbidity is above threshold for safe use ")
         avgturb = 1.1992
-        calculate_minimum_time()
     if (avgturb < 1):
         print ("     Turbidity is safe for use. \n" )
-
-def calculate_minimum_time():
-    hour = 0
-    hour  = math.log((1/avgturb),0.98)
     print ("     Minimum time required to return below a safe threshold = ",hour, "\n")
-
+    
+    
+}
 def main():
 
     with open('turbidity_data.json', 'r') as f:
         datavec = json.load(f)
 
-    calculate_turbidity(datavec['turbidity_data'])
+    turb  = calculate_turbidity(datavec['turbidity_data'])
+    time = calculate_minimum_time()
+    printstuff(turb, time)
 
 
 if __name__ == '__main__':
